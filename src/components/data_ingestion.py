546302e647b192349dd5260c68333920ecb872c8
Pritam Tung <pritamtung03@gmail.com>
2026-02-13 22:26:31 +0530

    add the code of data_transformation.py file and utils file and creat the pikles file in artifacts

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -7,6 +7,10 @@ from src.logger import logging
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
+
+from src.components.data_transformation import dataTransformation
+from src.components.data_transformation import dataTransformationConfig
+
 class DataIngestionConfig:
     train_data_path: str = os.path.join('artifacts', 'train.csv')
     test_data_path: str = os.path.join('artifacts', 'test.csv')
@@ -41,4 +45,6 @@ class DataIngestion:
             raise CustomException(e, sys)
 if __name__ == "__main__":
     obj = DataIngestion()
-    obj.initiate_data_ingestion()
+    train_data , test_data = obj.initiate_data_ingestion()
+    data_transformation = dataTransformation()
+    data_transformation.initiate_data_transformation(train_data, test_data)
